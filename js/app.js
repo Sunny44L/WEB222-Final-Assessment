150d8b84d2d634f3782515afdc8756e02f871c72
Sunny Long <123114863+Sunny44L@users.noreply.github.com>
2023-04-10 17:18:38 -0400

    Final Assessment
    Aeroplane Chess website

---
+++ js/app.js
@@ -0,0 +1,103 @@
+const {reviewData} =window;
+
+function createCards(review){
+    var container = document.getElementById("cards");
+
+    var card = document.createElement("div");
+    card.style.margin = "5px";
+    card.style.color = "#212121";
+    card.style.borderRadius = "10px";
+    card.style.width = "250px";
+    card.style.height = "200px";
+    card.style.backgroundColor = "#f47c7c";
+    card.style.float = "left";
+
+    var name = document.createElement('h5');
+    var nameT = document.createTextNode(review.name);
+    name.style.color = "#212121";
+    name.style.margin = "5px";
+    name.style.paddingTop = '10px';
+    name.appendChild(nameT);
+    
+    var date = document.createElement("time");
+    var dateT = new Date(review.date).toLocaleDateString();
+    date.style.margin = "5px";
+    date.style.backgroundColor = "#f47c7c";
+    date.innerHTML = dateT;
+
+    var rating = document.createElement('p');
+    var ratingT = document.createTextNode("Rating: ");
+    for(var i = 0; i < review.rating ; i++){
+        ratingT.nodeValue += "★";
+    }
+    for(var i = 5; i > review.rating; i--){
+        ratingT.nodeValue += "☆";
+    }
+    rating.style.margin = "5px";
+    rating.appendChild(ratingT);
+
+    var revw = document.createElement('p');
+    var revwT = document.createTextNode(review.review);
+    revw.style.margin = "5px";
+    revw.appendChild(revwT);
+
+    card.appendChild(name);
+    card.appendChild(date);
+    card.appendChild(rating);
+    card.appendChild(revw);
+
+    container.appendChild(card);
+}
+
+window.reviewData.forEach(createCards);
+
+
+window.validateNewCard = function(){
+    //prevent the page from refreshing
+    var form = document.getElementById('newreview');
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+})
+
+const nameT = document.getElementById('name').value.trim();
+const rating = document.querySelector('input[name = "rating"]:checked');
+const review = document.getElementById('review').value.trim();
+
+if(nameT ==="" || nameT.length > 20){
+    var err = document.getElementById("errName");
+    err.style.display = "block";
+    return false;
+}
+if(!rating){
+    var err = document.getElementById("errRating");
+    err.style.display = "block";
+    return false;
+}
+if(review ===""){
+    var err = document.getElementById("errReview");
+    err.style.display = "block";
+    return false;
+}
+
+//if validation success, continue to generate new card
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth() + 1;
+const day = currentDate.getDate();
+const date = `${year}-${month}-${day}`;
+
+var container = document.getElementById("cards");
+container.innerHTML = "";
+
+const newReview = {
+    name: nameT,
+    date: date,
+    rating: rating.value,
+    review: review
+}
+
+window.reviewData.push(newReview);
+window.reviewData.forEach(createCards);
+
+return false;
+}
---
+++ js/data.js
@@ -0,0 +1,34 @@
+window.reviewData  = [
+	/* Your Review Data Goes Here… */
+    {
+        name:"Sunny",
+        date:"2023-04-10",
+        rating: "4",
+        review: "I am so bad at rolling the 6, it is so hard to get my planes started."
+    },
+    {
+        name:"Erik",
+        date:"2022-03-18",
+        rating: "5",
+        review: "My favorite childhood game."
+    },
+    {
+        name:"Minjay",
+        date:"2022-11-28",
+        rating: "1",
+        review: "OMG! My chess pieces kept getting eaten...."
+    },
+    {
+        name:"Kenshi",
+        date:"2021-12-24",
+        rating: "5",
+        review: "This is the perfect party game."
+    },
+    {
+        name:"Alize",
+        date:"2023-01-03",
+        rating: "3",
+        review: "This game should set a new rule to let the pieces out of the hanger."
+    }
+];
+
